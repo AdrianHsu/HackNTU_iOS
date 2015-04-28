a13ab0c95d0f171f84d8b955012351af4417299f
AdrianHsu <adrianhsu1995@gmail.com>
2015-04-28 19:00:34 +0800

    TEST

---
+++ iOS_week3 - Adrian_Hsu/iOS_week3 - Adrian_Hsu/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  iOS_week3 - Adrian_Hsu
+//
+//  Created by AdrianHsu on 2015/4/28.
+//  Copyright (c) 2015年 AdrianHsu. All rights reserved.
+
+// 1. 使用 NSUserDefault 儲存調色盤顏色
+// 2. 使用 UIWebView 打造自己專屬瀏覽器
+// mail 到：appletone.tw@gmail.com
+// 標題：[HackCourse] iOS week3 - 姓名
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

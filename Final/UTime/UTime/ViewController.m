8c43edccb8461b10ef747f50aedc1e31df0f0d37
AdrianHsu <adrianhsu1995@gmail.com>
2015-05-07 17:28:32 +0800

    FBSDKCoreKit FBLoginButton done!

---
+++ Final/UTime/UTime/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  UTime
+//
+//  Created by AdrianHsu on 2015/5/7.
+//  Copyright (c) 2015年 AdrianHsu. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
+    loginButton.center = self.view.center;
+    [self.view addSubview:loginButton];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
